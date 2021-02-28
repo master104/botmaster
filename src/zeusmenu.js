58ed13f1bb4d330f70ef916bd624744347624c65
master104 <mixmaster39358@gmail.com>
2021-02-28 01:31:39 -0300

    First commit

---
+++ src/zeusmenu.js
@@ -0,0 +1,40 @@
+const mastermenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Master:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  FEITO POR *MASTER*
+  DUVIDAS? 👇
+  WA.me/5588994525375
+╚════════════════════`
+}
+
+exports.mastermenu = zeusmenu
+
+
+
+
+
+
+
+
